474bbc2946a2edbf2f7994d09347f312b2725a0d
Felix Ochoa <fdavidochoa@unicesar.edu.co>
2023-05-27 08:09:03 -0500

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import userRoutes from "./routes/users.routes.js";
 import matterRoutes from "./routes/matters.routes.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(userRoutes, matterRoutes);
 
